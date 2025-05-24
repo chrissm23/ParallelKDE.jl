57dcbab1d0c8a759a2efb05818969524f9f9c090
Documenter.jl <documenter@juliadocs.github.io>
2025-05-24 16:52:23 +0000

    build based on f441e66

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ParallelKDE","category":"page"},{"location":"#ParallelKDE","page":"Home","title":"ParallelKDE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ParallelKDE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ParallelKDE]","category":"page"}]
+[{"location":"#ParallelKDE","page":"Home","title":"ParallelKDE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ParallelKDE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
