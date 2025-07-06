9745fcdcd0fd3de226db659de7803394ce0b78f4
Documenter.jl <documenter@juliadocs.github.io>
2025-07-06 15:56:28 +0000

    build based on f288cb1

---
+++ v1.0.0-alpha/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ParallelKDE","page":"Home","title":"ParallelKDE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ParallelKDE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
